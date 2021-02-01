df4931e81dc2b0c652c095004bae9432e84aa1ba
LaTricia Hill <78131770+MoonlightKosmos7@users.noreply.github.com>
2021-02-01 16:34:56 -0700

    first commit

---
+++ src/App.java
@@ -0,0 +1,27 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName="LaTricia";
+				System.out.println(firstName);
+				String middleName="Anne";
+				System.out.println(middleName);
+				String lastName="Hill";
+				int age=24;
+				int friends=10;
+				System.out.println(lastName);
+				System.out.println("Hello, my name is"+" "+firstName+ " "+middleName+" "+lastName);
+				System.out.println("I am"+" "+age+" "+"years old");
+				System.out.println("At"+" "+age+" "+"I have"+" "+friends+" "+"friends");
+				String newItem="iPhone 12";
+				int cost=1200;
+				int accBal=1000;
+				int endingBal=cost-accBal;
+				
+				System.out.println("I bought the new"+" "+newItem+" "+"and it was"+" "+cost);
+				System.out.println("My account had"+" "+accBal);
+				System.out.println("After buying it"+" "+"I had overdrawn my account by"+" "+endingBal);
+				}
+	
+}
